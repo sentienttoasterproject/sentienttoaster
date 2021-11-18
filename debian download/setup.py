c122596ba397603969de1c49b4f38f3e45ad7891
sentienttoasterproject <94564661+sentienttoasterproject@users.noreply.github.com>
2021-11-17 17:44:42 -0700

    Add files via upload

---
+++ debian download/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name = "sentient toaster program",
+    version = "1.0",
+    author_email = "sentienttoasterproject@gmail.com",
+    description = "simple rock paper scissors guesser with a bit of randomness",
+   
+    url = "https://github.com/sentienttoasterproject/sentienttoaster",
+    packages=['sentienttoasterproject'],
+  
+    
+    classifiers=[
+        "",
+    ],
+)
+
